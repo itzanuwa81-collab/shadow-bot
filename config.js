e62dac7c8c291ab0fa6784d1e3f9c6613cde1e8a
itzanuwa81-collab <itzanuwa81@gmail.com>
2026-09-20 12:46:23 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  BOT_NAME: "SHADOW",
+  OWNER_NAME: "Anuhas",
+  BOT_IMAGE: "https://cloud.laksidu.site/dl/GDJVzlFfy4/lakiya-1789767553048.jpg",
+  DEFAULT_PREFIX: ".",
+  FOOTER: "> *👻 𝐒ʜᴀᴅᴏᴡ ✘ 𝐌ᴅ 𝐌ɪɴɪ 𝐁ᴏᴛ ║<<<*"
+};
